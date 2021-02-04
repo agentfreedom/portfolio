dd139bf2cafd14db15daacb5a7c84c244d6c2da4
agentfreedom <agentfreedom@zoho.com>
2021-02-04 18:27:12 +0200

    Adapted the section about me

--- work/js/accordion.js
+++ work/js/accordion.js
@@ -18,7 +18,7 @@ $(function () {
 	// функция удаления классов тегов h3 и p
 	function removeAccordion() {
 		$('#description .description__title').removeClass('accordion');
-		$('#description .description__text').removeClass('panel');
+		$('#description .description__blok').removeClass('panel');
 	}
 
 	// //запуск функции removeAccordion() если экран гаджета больше 700px
@@ -31,7 +31,7 @@ $(function () {
 	// функция добавляет новые классы тегам h3 и p
 	function addAccordion() {
 		$('#description .description__title').addClass('accordion');
-		$('#description .description__text').addClass('panel');
+		$('#description .description__blok').addClass('panel');
 	}
 	// //запуск функции addAccordion() если экран гаджета меньше 700px
 	// $(document).ready(function () {
